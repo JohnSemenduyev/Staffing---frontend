79f02bafa25d9bf374d4dea492d649872197c6a4
ashishcoderoofs <141501346+coderoofsit@users.noreply.github.com>
2025-08-14 09:01:03 -0400

    Updated Calender Misplacement

--- src/GraphqlClient.ts
+++ src/GraphqlClient.ts
@@ -1,4 +1,4 @@
 // src/graphqlClient.ts
 import { GraphQLClient } from "graphql-request";
-//export const graphQLClient = new GraphQLClient("https://securitywebappbackend.onrender.com/");
- export const graphQLClient = new GraphQLClient("http://localhost:4000/");
+export const graphQLClient = new GraphQLClient("https://securitywebappbackend.onrender.com/");
+//  export const graphQLClient = new GraphQLClient("http://localhost:4000/");
