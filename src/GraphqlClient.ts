bc5fdee06fa38d4cf8d87c35f37c02503c4f8f81
ashishcoderoofs <141501346+coderoofsit@users.noreply.github.com>
2025-09-23 11:47:58 +0530

    server shifted to paid server

--- src/GraphqlClient.ts
+++ src/GraphqlClient.ts
@@ -1,8 +1,8 @@
 // src/graphqlClient.ts
 import { GraphQLClient } from "graphql-request";
-// export const graphQLClient = new GraphQLClient("https://securitywebappbackend-fkuv.onrender.com/");
+export const graphQLClient = new GraphQLClient("https://securitywebappbackend-fkuv.onrender.com/");
 // export const graphQLClient = new GraphQLClient("https://maximal-security-coderoofs.nn.r.appspot.com/");
 // export const graphQLClient = new GraphQLClient("http://localhost:4000/");
-export const graphQLClient = new GraphQLClient("https://securitywebappbackend-v2.onrender.com/");
+// export const graphQLClient = new GraphQLClient("https://securitywebappbackend-v2.onrender.com/");
 
 // https://securitywebappbackend.onrender.com/
