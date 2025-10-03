4ca8392a5659347389077ddda276c5a5b8fb4194
ashishcoderoofs <coderoofitsolutions@gmail.com>
2025-10-03 01:32:13 -0400

    switched to paid server

--- src/GraphqlClient.ts
+++ src/GraphqlClient.ts
@@ -7,9 +7,9 @@ export function setGraphQLToken(token: string | null) {
 }
 // src/graphqlClient.ts
 import { GraphQLClient } from "graphql-request";
-// export const graphQLClient = new GraphQLClient("https://securitywebappbackend-fkuv.onrender.com/");
+export const graphQLClient = new GraphQLClient("https://securitywebappbackend-fkuv.onrender.com/");
 // export const graphQLClient = new GraphQLClient("https://maximal-security-coderoofs.nn.r.appspot.com/");
 // export const graphQLClient = new GraphQLClient("http://localhost:4000/");
-export const graphQLClient = new GraphQLClient("https://securitywebappbackend-v2.onrender.com/");
+// export const graphQLClient = new GraphQLClient("https://securitywebappbackend-v2.onrender.com/");
 
 // https://securitywebappbackend.onrender.com/
