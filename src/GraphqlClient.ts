01be3847cb839cbe7b9ba9fceb0b8c028573d49f
ashishcoderoofs <141501346+coderoofsit@users.noreply.github.com>
2025-11-06 19:28:21 +0530

    Update GraphqlClient.ts
    url fixed

--- src/GraphqlClient.ts
+++ src/GraphqlClient.ts
@@ -7,8 +7,8 @@ export function setGraphQLToken(token: string | null) {
 }
 // src/graphqlClient.ts
 import { GraphQLClient } from "graphql-request";
-// export const graphQLClient = new GraphQLClient("https://securitywebappbackend-fkuv.onrender.com/");
+export const graphQLClient = new GraphQLClient("https://securitywebappbackend-fkuv.onrender.com/");
 // export const graphQLClient = new GraphQLClient("http://localhost:4000/");
-export const graphQLClient = new GraphQLClient("https://securitywebappbackend-v2.onrender.com/");
+// export const graphQLClient = new GraphQLClient("https://securitywebappbackend-v2.onrender.com/");
 
 // https://securitywebappbackend.onrender.com/
