f209175a34d50ad88b6fc8fbe7150d1e53abe960
Ujjwal Nagpal <ujjwalnagpal3@gmail.com>
2026-07-27 01:18:56 +0530

    ok

--- src/GraphqlClient.ts
+++ src/GraphqlClient.ts
@@ -7,8 +7,6 @@ export function setGraphQLToken(token: string | null) {
 }
 // src/graphqlClient.ts
 import { GraphQLClient } from "graphql-request";
-//  export const graphQLClient = new GraphQLClient("https://securitywebappbackend-fkuv.onrender.com/");
-export const graphQLClient = new GraphQLClient("http://localhost:4000/");
-// export const graphQLClient = new GraphQLClient("https://securitywebappbackend-v2.onrender.com/");
+export const graphQLClient = new GraphQLClient("https://staffing-backend-4nm9.onrender.com/");
 
 // https://securitywebappbackend.onrender.com/ssss
